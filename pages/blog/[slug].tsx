4a1d5f9e04977e756221b5d510e9d2aeaa3126a8
Zurda <m.weizman@gmail.com>
2022-02-07 08:30:24 +0000

    Add mdx blog

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,73 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import {serialize} from 'next-mdx-remote/serialize'
+import {MDXRemote} from 'next-mdx-remote'
+import Head from 'next/head'
+import { Pane, Heading, majorScale } from 'evergreen-ui'
+
+import Container from '../../components/Container'
+import HomeNav from '../../components/HomeNav'
+
+
+export default function BlogPost( {source, frontMatter} ): InferGetStaticPropsType<typeof getStaticProps> {
+    return (
+    <Pane>
+      <Head>
+        <title>{`Yaba Blog | ${frontMatter.title}`}</title>
+        <meta name="description" content={frontMatter.summary} />
+      </Head>
+      <header>
+        <HomeNav />
+      </header>
+      <main>
+        <Container>
+          <Heading fontSize="clamp(2rem, 8vw, 6rem)" lineHeight="clamp(2rem, 8vw, 6rem)" marginY={majorScale(3)}>
+            {frontMatter.title}
+          </Heading>
+          <img src={frontMatter.mainImageUrl} width='500' />
+          <Pane>
+            <MDXRemote {...source} />
+          </Pane>
+        </Container>
+      </main>
+    </Pane>
+    )
+}
+
+// The returned value from `getStaticPaths` gets passed into `getStaticProps`
+// Gets the slug so we know the slug and path of the blog post
+// Async because reading and mapping through files take time but this is done at build time
+export const getStaticPaths: GetStaticPaths = async () => {
+    // Get the posts folder from `/posts` at the root directory
+    const postsDirectory = path.join(process.cwd(), 'posts')
+    const filenames = fs.readdirSync(postsDirectory)
+
+    // Map through the files, for each fileName return a params object which includes the slug
+    // remove the `mdx` extension
+    const paths = filenames.map((name) => ({ params: { slug: name.replace('.mdx', '') } }))
+
+    // Fallback purpose: if the `slug` doesn't exist -> 404 page
+    return { paths, fallback: false }
+}
+
+type Params = {
+    [param: string]: any
+}
+
+// The returned value from `getStaticProps` gets passed into the BlogPost
+// Gets the content of the blog post
+// Async because reading files take time but this is done at build time
+export const getStaticProps: GetStaticProps<Params> = async ({ params: { slug } }: Params) => {
+    // Get the file of the current slug
+    const postPath = path.join(process.cwd(), 'posts', `${slug}.mdx`)
+    const postFile = fs.readFileSync(postPath, 'utf-8')
+
+    // Use the gray-matter and next-mdx-remote to read the metadata and content
+    const { content, data } = matter(postFile)
+
+    const mdxSource = await serialize(content, { scope: data })
+
+    return { props: { source: mdxSource, frontMatter: data } }
+}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,14 +1,45 @@
-import { Pane } from 'evergreen-ui'
+import { Pane, majorScale } from 'evergreen-ui'
+import matter from 'gray-matter'
+import path from 'path'
+import fs from 'fs'
+import orderby from 'lodash.orderby'
+import Container from '../../components/Container'
 import HomeNav from '../../components/HomeNav'
+import PostPreview from '../../components/PostPreview'
 
-const Blog = () => {
+export default function Blog({ posts }) {
   return (
     <Pane>
       <header>
         <HomeNav />
       </header>
+      <main>
+        <Container>
+          {posts.map((post) => (
+            <Pane key={post.title} marginY={majorScale(5)}>
+              <PostPreview post={post} />
+            </Pane>
+          ))}
+        </Container>
+      </main>
     </Pane>
   )
 }
 
-export default Blog
+export async function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), 'posts')
+  const filenames = fs.readdirSync(postsDirectory)
+
+  const posts = orderby(
+    filenames.map((filename) => {
+      const filePath = path.join(postsDirectory, filename)
+      const fileContents = fs.readFileSync(filePath, 'utf8')
+      const { data } = matter(fileContents)
+      return { ...data, slug: filename.replace('.mdx', '') }
+    }),
+    ['publishedOn'],
+    ['desc']
+    )
+
+  return { props: { posts } }
+}
